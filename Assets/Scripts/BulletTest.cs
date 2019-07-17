ca8fa49c2bd754c8a10b702ca0282b24d6ddc68f
Peter <hguanqi7@gmail.com>
2019-07-17 12:43:32 +1200

    Test shooting

---
+++ Assets/Scripts/BulletTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTest : MonoBehaviour
+{
+    
+    public int bspeed = 8;
+    private int a = 0;
+    private int b = 1;
+
+
+    public GameObject owner { get; private set; }
+
+
+    private Vector2 direction;
+
+    public Rigidbody2D rb;
+    public float speed = 8f;
+    public float lifetime = 1f;
+
+    public void Create(GameObject owner, Vector2 direction)
+    {
+        this.owner = owner;
+        this.direction = direction;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+      
+
+
+
+        Destroy(this.gameObject, lifetime);
+
+        rb.velocity = direction * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void OnTriggerEnter2D(Collider2D c)
+    {
+
+        Debug.Log("BULLET COLLIDE!!!!!");
+
+        if (c.gameObject == this.owner)
+            return;
+
+        GameObject.Destroy(this.gameObject);
+
+        
+    }
+}
---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class InputData
+{
+    public KeyCode Left;
+    public KeyCode Right;
+    public KeyCode Up;
+    public KeyCode Down;
+
+    public KeyCode Jump;
+    public KeyCode ShootR;
+    public KeyCode ShootL;
+    public KeyCode ShootUp;
+    public KeyCode ShootDown;
+
+}
+
+[System.Serializable]
+public class CharaData
+{
+
+    //public int charaset;
+    public float hp = 500;
+    public float dmg;
+    public float spd;
+    
+
+}
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public InputData m_Input;
+    //Shoot
+    public BulletTest Bullet;
+   // public GameObject Bullet1;
+    //public GameObject Bullet2;
+    private float nextFire = 0.3f;
+    private float myTime = 0.0F;
+    public float fireDelta = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Shoot
+        myTime = myTime + Time.deltaTime;
+      
+            // SHOOT
+            if (Input.GetKeyDown(m_Input.ShootR) && myTime > nextFire)
+            {
+                // CREATE
+                var bullet = Instantiate(Bullet, transform.position, transform.rotation);
+
+                // DIRECTION
+                Vector2 direction = Vector2.right;
+                
+
+           
+
+                // CREATE
+                bullet.Create(this.gameObject, direction);
+
+                // NEXT FIRE
+                nextFire = fireDelta;
+                myTime = 0.0f;
+            
+            }
+
+        if (Input.GetKeyDown(m_Input.ShootL) && myTime > nextFire)
+        {
+            // CREATE
+            var bullet = Instantiate(Bullet, transform.position, transform.rotation);
+
+            // DIRECTION
+            Vector2 direction = Vector2.left;
+
+
+
+
+            // CREATE
+            bullet.Create(this.gameObject, direction);
+
+            // NEXT FIRE
+            nextFire = fireDelta;
+            myTime = 0.0f;
+
+        }
+        if (Input.GetKeyDown(m_Input.ShootUp) && myTime > nextFire)
+            {
+               
+                var bullet = Instantiate(Bullet, transform.position, transform.rotation);
+
+            
+
+                // CREATE
+                Vector2 direction = Vector2.up;
+                bullet.Create(this.gameObject, direction);
+
+                // NEXT FIRE
+                nextFire = fireDelta;
+                myTime = 0.0f;
+            }
+            if (Input.GetKeyDown(m_Input.ShootDown) && myTime > nextFire)
+            {
+               
+                var bullet = Instantiate(Bullet, transform.position, transform.rotation);
+
+           
+
+                // CREATE
+                Vector2 direction = Vector2.down;
+                bullet.Create(this.gameObject, direction);
+
+                // NEXT FIRE
+                nextFire = fireDelta;
+                myTime = 0.0f;
+            }
+
+        }
+    
+}
