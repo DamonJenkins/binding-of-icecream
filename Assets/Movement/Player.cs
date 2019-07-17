50eb7154ab627c2bc97506e5588af83c1413a942
Damon Jenkins <damonsfjenkins@gmail.com>
2019-07-17 12:33:39 +1200

    Basic Movement

---
+++ Assets/Movement/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //float velThisFrame = moveSpeed;
+
+        transform.position += new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+        ) * moveSpeed * Time.deltaTime;
+    }
+}
