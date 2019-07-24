d348a95a29de5e4b7fd2b7edcb5db150e44408fe
Peter <hguanqi7@gmail.com>
2019-07-24 15:02:11 +1200

    Added Menu

---
+++ Assets/Menu Stuff/MenuScr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Sprites;
+using UnityEngine.UI;
+public class MenuScr : MonoBehaviour
+{
+    public Image Control;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        ExitGame();
+    }
+    public void ShowCtrl()
+    {
+        Control.enabled = !Control.enabled;
+    }
+}
